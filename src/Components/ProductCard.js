e9285624f79359498367d1a90820568226b84d62
sharon <sharon.rueda@ultertechnologies.com>
2022-06-07 19:56:09 -0500

    primer y segundo problema

---
+++ src/Components/ProductCard.js
@@ -0,0 +1,14 @@
+const ProductCard = () => {
+    const view = `
+    <article class="product">
+        <img src"images[0]" alt="title-image-i"/>
+        <h2>
+        title
+        <small>$ price</small>
+        </h2>
+    </article>
+    `
+    return view
+}
+
+export default ProductCard;
--- src/index.js
+++ src/index.js
@@ -2,30 +2,39 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+const getData = async api => {
+    fetch(api)
+        .then(response => response.json())
+        .then(response => {
+            let products = response;
+            let output = products.map(product => {
+                return `<article class="Card">
+                <img src="${product.images[0]} alt="${product.title}"/>
+                <h2>${product.title}<small>$ ${product.price}</small></h2>
+              </article>`;
+            });
+            let newItem = document.createElement('section');
+            newItem.classList.add('Item');
+
+            newItem.innerHTML = output;
+            $app.appendChild(newItem);
+            return products
+        })
+        .catch(error => console.log(error));
+    const res = await fetch(api)
+    const data = await res.json()
+    return data
 }
 
 const loadData = () => {
-  getData(API);
+    return getData(API);
 }
+loadData()
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+    // logic...
 }, {
-  rootMargin: '0px 0px 100% 0px',
+    rootMargin: '0px 0px 100% 0px',
 });
 
 intersectionObserver.observe($observe);
